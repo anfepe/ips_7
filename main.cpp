d4f3d3d72cb5d98ef2dacbbbb9c04f766d6c8e5a
Anastasia Petrova <ahest@mail.ru>
2017-12-09 18:46:45 +0300

    Task 5

--- main.cpp
+++ main.cpp
@@ -15,12 +15,15 @@ double par(void)
 	double S = 0.0;
 	step = 1.0 / (double)num;
 	double t = omp_get_wtime();
+	omp_lock_t writelock;
+	omp_init_lock(&writelock);
 	for (i = 0; i < num; i++)
 	{
 		x = (i + 0.5)*step;
 		S = S + 4.0 / (1.0 + x*x);
-		#pragma omp critical
+		omp_set_lock(&writelock);
 		inc++;
+		omp_unset_lock(&writelock);
 	}
 	t = omp_get_wtime() - t;
 	pi = step * S;
